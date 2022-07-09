bce756333c8b7bd62a971220a095008403b92734
Abdelghafour <ghafourragg@gmail.com>
2022-07-09 23:03:02 +0100

    added mui icons, made basic Attribution

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./Dist/App.css";
 import Random from "./Components/Random";
 import Homepage from "./Routes/Homepage";
+import Attribution from "./Components/Attribution";
 
 function App() {
   return (
     <main className="App">
       {/* <Random /> */}
       <Homepage />
+      <Attribution />
     </main>
   );
 }
--- src/Components/Attribution.jsx
+++ src/Components/Attribution.jsx
@@ -1,12 +1,24 @@
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import React from "react";
 
 const Attribution = () => {
   return (
     <Box>
-      <Typography variant="p">Coded with </Typography>
-      <Typography></Typography>
+      <Typography variant="p" component="p" fontFamily="ROBOTO">
+        Coded with <FavoriteIcon color="error" /> By
+        <Typography
+          variant="a"
+          component="a"
+          href="https://github.com/Abdelghafour122"
+          target="_blank"
+          rel="noreferrer noopener"
+          color="primary"
+        >
+          Abdelghafour122
+        </Typography>
+      </Typography>
     </Box>
   );
 };
